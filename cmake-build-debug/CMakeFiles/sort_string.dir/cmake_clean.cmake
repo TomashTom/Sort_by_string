48bef6d9a7ecb956cf647d702e3e3005f0081ee8
TomashTom <smolskijt@gmail.com>
2021-06-06 20:40:20 +0300

    First commit

---
+++ cmake-build-debug/CMakeFiles/sort_string.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sort_string.dir/main.cpp.obj"
+  "libsort_string.dll.a"
+  "sort_string.exe"
+  "sort_string.exe.manifest"
+  "sort_string.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sort_string.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
